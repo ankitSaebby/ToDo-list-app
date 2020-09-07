888a9b217b6048200026990b6c3fc0c4b972e705
Ankit singh <ankit.aebby@gmail.comgit config --global user.name ankitSaebby>
2020-09-07 11:14:34 +0530

    functionality

--- app.js
+++ app.js
@@ -4,36 +4,34 @@ const express = require ("express");
 const bodyParser = require ("body-parser");
 
 const app=express();
+app.use(bodyParser.urlencoded({extened:true}));
 
 app.set("view engine","ejs");
+
+var items=[];
 
 app.get("/",function(req,res){
   var today=new Date();
 
   var options={
-    weekend:"long";
-    day:"numeric";
-    month:"long";
+    weekday : "long",
+    day:"numeric",
+    month:"long"
   };
 
+  var day = today.toLocaleDateString("en-US",options);
 
-
-
-  // if(currentDay===0){
-  //   day="sunday";
-  //   // res.sendFile(__dirname+"/weekend.html");
-  //   //res.render("list", {kindOfDay: day});
-  // }
-  // else{
-  //   day="week day";
-  //   //res.sendFile(__dirname+"/index.html");
-  //   //res.render("list", {kindOfDay: day});
-  // }
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day , newListItem:items});
 });
 
+app.post("/",function(req,res){
+  var item= req.body.newItem;
+  items.push(item);
+  //console.log(item);
+  res.redirect("/");
+})
 
 
 app.listen(3000,function(){
   console.log("server up and running on port 3000");
-})
+});
